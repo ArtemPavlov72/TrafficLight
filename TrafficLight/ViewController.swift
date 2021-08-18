a9d6fa06257694a874fe2ffffaaf0de146eec399
Artem Pavlov <pavlov.tema@gmail.com>
2021-08-18 13:09:48 +0500

    View control

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var orangeLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var showTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 80
+        orangeLight.layer.cornerRadius = 80
+        greenLight.layer.cornerRadius = 80
+        redLight.alpha = 0.3
+        orangeLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        showTextButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func showTextButtonPrest() {
+        
+        if redLight.alpha == 0.3 {
+            showTextButton.setTitle("START", for: .normal)
+        } else {
+            showTextButton.setTitle("men", for: .normal)
+        }
+    }
+    
+    
 }
 
